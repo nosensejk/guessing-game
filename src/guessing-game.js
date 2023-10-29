cc5f16ba6043feb31260a77d78ef78274587fc7b
Anton Tur <antoxatur@gmail.com>
2023-10-29 18:31:38 +0300

    fix: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.avg = Math.round((this.min + this.max) / 2);
+        return this.avg;
     }
 
     lower() {
-
+        this.max = this.avg;
     }
 
     greater() {
-
+        this.min = this.avg;
     }
 }
 
